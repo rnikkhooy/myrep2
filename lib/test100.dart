e52cc2566415f9c8409412c08fe693d20f7b3ca7
rnikkhooy <rnikkhooy@gmail.com>
2023-04-05 19:13:20 +0330

    m1

---
+++ lib/test100.dart
@@ -0,0 +1,19 @@
+library test100;
+
+import 'package:flutter/material.dart';
+
+class pr extends StatelessWidget {
+  const pr({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('111111'),
+      ),
+      body: Center(
+        child: Text('22222222'),
+      ),
+    );
+  }
+}
